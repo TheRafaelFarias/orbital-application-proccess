fe46a032a451c2e2bd6abf71b0fa6b62998f43ff
Rafael Farias <rafaelfarias@zohomail.com>
2023-07-01 09:02:42 -0300

    feat(cta-button-component): add disabled and type from html button element fields into cta button

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -1,11 +1,15 @@
 const CTAButton: React.FC<{
   name: string;
   action?: () => any;
-  className: string;
-}> = ({ name, className }) => {
+  className?: string;
+  disabled?: boolean;
+  type?: HTMLButtonElement['type'];
+}> = ({ name, disabled, type = 'button', className = '' }) => {
   return (
     <button
-      className={`px-12 md:px-24 rounded-lg md:rounded-xl py-1 bg-white text-highlight text-base md:text-2xl ${className}`}
+      disabled={disabled}
+      type={type}
+      className={`px-12 md:px-24 rounded-lg md:rounded-xl py-1 bg-white text-highlight text-base md:text-2xl disabled:text-opacity-70 ${className}`}
     >
       {name}
     </button>
