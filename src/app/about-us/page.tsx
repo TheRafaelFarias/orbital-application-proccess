8cf1e8d49e5f5be13387d9f15a4dca5a9d95a1a7
Rafael Farias <rafaelfarias@zohomail.com>
2023-06-30 23:40:35 -0300

    feat: create about page

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,5 +1,79 @@
 const AboutUs: React.FC = () => {
-  return <div />;
+  return (
+    <section className="text-center ml-8 mr-4 md:ml-40 md:mr-32 overflow-y-auto mb-10">
+      <h1 className="text-highlight text-4xl md:text-5xl mb-4">A little more about us</h1>
+      <p className="text-xl font-description text-justify font-normal mr-4 md:mr-6">
+        Lorem ipsum dolor sit amet consectetur. Nunc tortor a etiam turpis.
+        Egestas habitant magna nec ipsum faucibus eget scelerisque vulputate.
+        Nunc massa et laoreet mauris. In placerat lacus ultricies tortor
+        placerat ornare neque dignissim. Imperdiet tincidunt in adipiscing sit
+        convallis porttitor sapien adipiscing faucibus. Ullamcorper integer ut
+        turpis neque quis lobortis. Habitasse purus viverra blandit habitant
+        volutpat. Justo varius sed feugiat volutpat ipsum velit donec. Ipsum
+        neque et orci interdum ultricies tristique lobortis. Faucibus sed tellus
+        aliquet sit tellus. Ut sed nunc egestas euismod turpis egestas. Vel
+        rhoncus viverra interdum quam. Fringilla et pretium in ac purus neque.
+        Vitae risus venenatis ipsum dolor sed a porta tortor viverra. Nec hac in
+        purus sed urna urna in arcu. Facilisi diam feugiat viverra auctor enim
+        et et bibendum pellentesque. Proin hac urna proin nec vitae erat
+        adipiscing. At at sed quis adipiscing. Purus odio dapibus suspendisse
+        aliquam curabitur. Odio integer in amet donec velit purus faucibus
+        imperdiet. Ipsum ornare dignissim turpis at purus malesuada leo sed
+        consequat. A quis enim ac praesent aliquam neque vitae. Lorem ipsum
+        tincidunt facilisis leo. Nisl velit fusce cursus blandit libero. A
+        integer vel nunc pretium. Aliquet morbi sed pulvinar nunc fermentum
+        maecenas lobortis aliquet. Velit nisl tempor lorem leo in dictum amet. A
+        aliquet suspendisse turpis sed odio nulla tellus. Massa magna aliquet
+        enim mattis pretium arcu. Ut et faucibus ultrices augue risus at amet
+        blandit at. Rhoncus vel cras nibh tortor justo. Eget semper lectus
+        scelerisque placerat lacus dolor. Risus id id sed id cras odio pharetra
+        dictumst. Ipsum sit pharetra varius vel ut augue volutpat erat. Id
+        sagittis orci ultrices enim interdum consectetur eget convallis. Sit
+        mattis viverra enim tellus est orci vitae et. Ultricies sed eget congue
+        consectetur cursus erat. Sit nec viverra amet neque nibh gravida sed.
+        Aenean velit consectetur nulla in in velit. Tellus pharetra metus ipsum
+        et et. Amet porttitor adipiscing quis laoreet faucibus dignissim magna.
+        Aliquet quis lectus viverra amet vulputate. Vivamus vulputate malesuada
+        ullamcorper ultrices augue tortor est. Platea vulputate ut amet pretium
+        faucibus netus. Vel a dignissim amet malesuada id. Enim a mi lobortis
+        varius integer ultrices congue tortor fringilla. Sagittis fermentum id
+        id quam faucibus felis. Odio pretium donec elit luctus mattis nulla
+        lorem malesuada. Fames massa vel urna et nisl. Nisi vestibulum tellus
+        scelerisque lorem faucibus non rutrum est neque. Pellentesque tellus sit
+        fermentum ut molestie tortor habitant turpis odio. Faucibus quis lorem
+        nulla vitae adipiscing interdum eget. Facilisis ultrices lorem porttitor
+        euismod adipiscing. Nibh ultrices porttitor duis eget varius urna
+        gravida. Elit sem sollicitudin luctus praesent diam magna magnis in.
+        Tellus enim tempor tincidunt ipsum leo aliquam morbi platea duis. Nec
+        dui accumsan pretium molestie sit risus donec senectus justo. Vivamus
+        vulputate malesuada ullamcorper ultrices augue tortor est. Platea
+        vulputate ut amet pretium faucibus netus. Vel a dignissim amet malesuada
+        id. Enim a mi lobortis varius integer ultrices congue tortor fringilla.
+        Sagittis fermentum id id quam faucibus felis. Odio pretium donec elit
+        luctus mattis nulla lorem malesuada. Fames massa vel urna et nisl. Nisi
+        vestibulum tellus scelerisque lorem faucibus non rutrum est
+        neque.Vivamus vulputate malesuada ullamcorper ultrices augue tortor est.
+        Platea vulputate ut amet pretium faucibus netus. Vel a dignissim amet
+        malesuada id. Enim a mi lobortis varius integer ultrices congue tortor
+        fringilla. Sagittis fermentum id id quam faucibus felis. Odio pretium
+        donec elit luctus mattis nulla lorem malesuada. Fames massa vel urna et
+        nisl. Nisi vestibulum tellus scelerisque lorem faucibus non rutrum est
+        neque.Vivamus vulputate malesuada ullamcorper ultrices augue tortor est.
+        Platea vulputate ut amet pretium faucibus netus. Vel a dignissim amet
+        malesuada id. Enim a mi lobortis varius integer ultrices congue tortor
+        fringilla. Sagittis fermentum id id quam faucibus felis. Odio pretium
+        donec elit luctus mattis nulla lorem malesuada. Fames massa vel urna et
+        nisl. Nisi vestibulum tellus scelerisque lorem faucibus non rutrum est
+        neque.Vivamus vulputate malesuada ullamcorper ultrices augue tortor est.
+        Platea vulputate ut amet pretium faucibus netus. Vel a dignissim amet
+        malesuada id. Enim a mi lobortis varius integer ultrices congue tortor
+        fringilla. Sagittis fermentum id id quam faucibus felis. Odio pretium
+        donec elit luctus mattis nulla lorem malesuada. Fames massa vel urna et
+        nisl. Nisi vestibulum tellus scelerisque lorem faucibus non rutrum est
+        neque.
+      </p>
+    </section>
+  );
 };
 
 export default AboutUs;
