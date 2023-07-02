5236a3decd2bca4a1c7c677ceb865532a76d49ad
Rafael Farias <rafaelfarias@zohomail.com>
2023-07-02 10:09:23 -0300

    fix(navbar): logo redirect to home page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 const Navbar: React.FC = () => {
@@ -7,9 +8,11 @@ const Navbar: React.FC = () => {
 
   return (
     <nav className="flex flex-col items-center sm:flex-row sm:justify-between p-6 pb-10 pr-10 w-full">
-      <div className="w-14 h-14 rounded-md bg-highlight text-white flex items-center justify-center text-3xl font-primary">
-        R
-      </div>
+      <Link href="/">
+        <div className="w-14 h-14 rounded-md bg-highlight text-white flex items-center justify-center text-3xl font-primary">
+          R
+        </div>
+      </Link>
       <div className="flex space-x-5 py-5 font-navbar text-2xl sm:text-3xl">
         <a
           href="/"
