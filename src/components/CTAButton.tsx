4f7d699a6131e8e3c825bfab64463062e60ae970
Rafael Farias <rafaelfarias@zohomail.com>
2023-07-02 10:01:11 -0300

    feat(cta-button-component): action field be executed in a onClick and add the option to pass custom styles

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -1,14 +1,19 @@
+import { CSSProperties } from 'react';
+
 const CTAButton: React.FC<{
   name: string;
   action?: () => any;
   className?: string;
   disabled?: boolean;
+  style?: CSSProperties;
   type?: HTMLButtonElement['type'];
-}> = ({ name, disabled, type = 'button', className = '' }) => {
+}> = ({ name, disabled, style, type = 'button', className = '', action }) => {
   return (
     <button
+      style={style!}
       disabled={disabled}
       type={type}
+      onClick={action}
       className={`px-12 md:px-24 rounded-lg md:rounded-xl py-1 bg-white text-highlight text-base md:text-2xl disabled:text-opacity-70 ${className}`}
     >
       {name}
